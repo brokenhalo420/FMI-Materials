1b5c20c23b8fc2d573d3f91ff1b6c8bde9594028
brokenhalo420 <m.nikolov1501@gmail.com>
2022-09-01 10:49:20 +0300

    postman dump and changes to the user repository

--- fmi-materials/src/main/java/com/project/materials/fmi/controllers/UserController.java
+++ fmi-materials/src/main/java/com/project/materials/fmi/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.project.materials.fmi.controllers;
 
+import com.project.materials.fmi.dtos.CourseDTO;
 import com.project.materials.fmi.dtos.UserDTO;
 import com.project.materials.fmi.repositories.services.UserRepositoryService;
 import io.swagger.annotations.Api;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -26,19 +29,19 @@ public class UserController {
         return service.getAllUsers();
     }
 
-    @GetMapping("/get-by-id/{id}")
-    public Optional<UserDTO> getUserById(@PathVariable long id){
-        return service.getUserById(id);
-    }
-
-    @GetMapping("/get/{email:.*}&{password:.*}")
-    public Optional<UserDTO> getUser(@PathVariable String email, @PathVariable String password){
+    @GetMapping("/get")
+    public Optional<UserDTO> getUser(@RequestParam String email, @RequestParam String password){
         return service.getUser(email,password);
     }
 
-    @GetMapping("/get-by-name/{name=.*}")
-    public Iterable<UserDTO> getUserByName(@PathVariable String name){
+    @GetMapping("/get-by-name")
+    public Iterable<UserDTO> getUserByName(@RequestParam String name){
         return service.getUsersByName(name);
+    }
+
+    @GetMapping("/get-by-email")
+    public UserDTO getUserByEmail(@RequestParam String email){
+        return service.getUserByEmail(email);
     }
 
     @PostMapping("/add")
@@ -46,15 +49,24 @@ public class UserController {
         service.addUser(user);
     }
 
-    @DeleteMapping("/delete/{email=.*}&{password=.*}")
-    public void deleteUser(@PathVariable String email, @PathVariable String password){
+    @DeleteMapping("/delete")
+    public void deleteUser(@RequestParam String email, @RequestParam String password){
         service.deleteUser(email, password);
     }
 
-    @DeleteMapping("/delete-by-id/{id}")
-    public void deleteUser(@PathVariable long id){
-        service.deleteUserById(id);
+    @GetMapping("/favorites")
+    public Iterable<CourseDTO> getFavorites(@RequestParam String email){
+        return this.service.getFavorites(email);
     }
 
+    @PutMapping("/favorites/add")
+    public void addToFavorites(@RequestParam String email, @RequestParam String courseName){
+        this.service.addToFavorites(email,courseName);
+    }
+
+    @PutMapping("/favorites/remove")
+    public void removeFromFavorites(@RequestParam String email, @RequestParam String courseName){
+        this.service.removeFromFavorites(email,courseName);
+    }
 
 }
--- fmi-materials/src/main/java/com/project/materials/fmi/repositories/services/UserRepositoryService.java
+++ fmi-materials/src/main/java/com/project/materials/fmi/repositories/services/UserRepositoryService.java
@@ -1,7 +1,10 @@
 package com.project.materials.fmi.repositories.services;
+import com.project.materials.fmi.dtos.CourseDTO;
 import com.project.materials.fmi.dtos.UserDTO;
+import com.project.materials.fmi.mappers.CourseMapper;
 import com.project.materials.fmi.mappers.UserMapper;
 import com.project.materials.fmi.models.User;
+import com.project.materials.fmi.repositories.contracts.CourseDB;
 import com.project.materials.fmi.repositories.contracts.UserDB;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,51 +13,87 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class UserRepositoryService {
-    private UserDB repository;
+    private UserDB userRepository;
+    private CourseDB courseRepository;
     
     @Autowired
-    public UserRepositoryService(UserDB repository){
-        this.repository = repository;
+    public UserRepositoryService(UserDB userRepository, CourseDB courseRepository){
+        this.userRepository = userRepository;
+        this.courseRepository = courseRepository;
     }
 
     public Iterable<UserDTO> getAllUsers() {
-        return repository.findAll().stream().map(UserMapper::toDTO).collect(Collectors.toList());
+        return userRepository.findAll().stream().map(UserMapper::toDTO).collect(Collectors.toList());
     }
 
     public Iterable<UserDTO> getUsersByName(String name){
-        return repository.findAll().stream().filter(user -> name.equals(user.getName()))
+        return userRepository.findAll().stream().filter(user -> name.equals(user.getName()))
                 .map(UserMapper::toDTO).collect(Collectors.toList());
     }
 
+    public UserDTO getUserByEmail(String email){
+        return userRepository.findAll().stream().filter(x -> email.equals(x.getEmail())).map(UserMapper::toDTO).findFirst().get();
+    }
+
     public Optional<UserDTO> getUser(String email, String password){
-        return repository.findAll().stream()
+        return userRepository.findAll().stream()
                 .filter(user -> email.equals(user.getEmail()) && password.equals(user.getPassword()))
                 .map(UserMapper::toDTO).findFirst();
     }
 
     public void addUser(UserDTO user){
         User entry = new User();
-        repository.save(UserMapper.fromDTO(entry,user));
-    }
-
-    public Optional<UserDTO> getUserById(long id){
-        return repository.findById(id).map(UserMapper::toDTO);
-    }
-
-    public void deleteUserById(long id){
-        repository.deleteById(id);
+        userRepository.save(UserMapper.fromDTO(entry,user));
     }
 
     public void deleteUser(String email, String password){
         User user;
         try{
-            user = repository.findAll().stream()
+            user = userRepository.findAll().stream()
                     .filter(x -> email.equals(x.getEmail()) && password.equals(x.getPassword()))
                     .findFirst().get();
         } catch (NoSuchElementException e){
             return;
         }
+        userRepository.delete(user);
+    }
 
-        repository.delete(user);
+    public Iterable<CourseDTO> getFavorites(String email){
+        return this.userRepository.findAll().stream().filter(x-> x.getEmail().equals(email)).findFirst().get().getFavorites()
+                .stream().map(CourseMapper::toDTO).collect(Collectors.toList());
+    }
+
+    public void addToFavorites(String email, String courseName){
+        try {
+            var user = this.userRepository.findAll().stream().filter(x -> x.getEmail().equals(email)).findFirst().get();
+            var course = this.courseRepository.findAll().stream().filter(x -> x.getName().equals(courseName))
+                    .findFirst().get();
+
+            user.getFavorites().add(course);
+            course.getFavorited().add(user);
+
+            this.userRepository.save(user);
+            this.courseRepository.save(course);
+        }
+        catch (NoSuchElementException e){
+            return;
+        }
+    }
+
+    public void removeFromFavorites(String email, String courseName){
+        try {
+            var user = this.userRepository.findAll().stream().filter(x -> x.getEmail().equals(email)).findFirst().get();
+            var course = this.courseRepository.findAll().stream().filter(x -> x.getName().equals(courseName))
+                    .findFirst().get();
+
+            user.getFavorites().remove(course);
+            course.getFavorited().remove(user);
+
+            this.userRepository.save(user);
+            this.courseRepository.save(course);
+        }
+        catch (NoSuchElementException e){
+            return;
+        }
     }
 }
